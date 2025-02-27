acff5a8ded63cc27284eefd5ba91ff4bcfebaa4a
Kevin Manka <113623001+kevin-manka@users.noreply.github.com>
2025-02-26 21:54:32 -0500

    fix: mesh upload schema

--- app/routes/hailgen.new.$id.mesh.upload/schema.ts
+++ app/routes/hailgen.new.$id.mesh.upload/schema.ts
@@ -1,36 +1,33 @@
 import { NodeOnDiskFile } from '@remix-run/node';
 import { z } from 'zod';
 
-export default z
-	.object({
-		pairAnalysis: z.enum(['true', 'false']).transform((value) => value === 'true'),
-		postMesh: z
-			.instanceof(NodeOnDiskFile, {
-				message: 'Please select a file.'
-			})
-			.refine((file) => {
-				return file.name.endsWith('.stl');
-			}, 'File should be of .stl type.')
-	})
-	.refine(
-		(data) => {
-			if (data.pairAnalysis) {
-				return z
-					.object({
-						preMesh: z
-							.instanceof(NodeOnDiskFile, {
-								message: 'Please select a file.'
-							})
-							.refine((file) => {
-								return file.name.endsWith('.stl');
-							}, 'File should be of .stl type.')
-					})
-					.safeParse(data).success;
-			}
-			return true;
-		},
-		{
-			message: 'Please select a .stl file for the pre-hit mesh.',
-			path: ['preMesh']
-		}
-	);
+const baseSchema = z.object({
+    pairAnalysis: z.enum(['true', 'false']).transform((value) => value === 'true'),
+    postMesh: z
+        .instanceof(NodeOnDiskFile, {
+            message: 'Please select a file.'
+        })
+        .refine((file) => {
+            return file.name.endsWith('.stl');
+        }, 'File should be of .stl type.')
+});
+
+const pairSchema = baseSchema.extend({
+    preMesh: z
+        .instanceof(NodeOnDiskFile, {
+            message: 'Please select a file.'
+        })
+        .refine((file) => {
+            return file.name.endsWith('.stl');
+        }, 'File should be of .stl type.')
+});
+
+const schema = z.union([
+    baseSchema.refine((data) => !data.pairAnalysis, {
+        message: 'Please select a .stl file for the pre-hit mesh.',
+        path: ['preMesh']
+    }),
+    pairSchema
+]);
+
+export default schema;
