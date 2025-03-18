f43d1daa5f58b2ab2a88750246abcc19e05ac5de
Kevin Manka <113623001+kevin-manka@users.noreply.github.com>
2025-03-17 22:06:56 -0400

    feat: light mode for loading indicator

--- app/components/ui/loading-indicator.tsx
+++ app/components/ui/loading-indicator.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { useTheme } from 'remix-themes';
 import csslLoading from '~/assets/cssl-loading.gif';
+import csslLoadingAlt from '~/assets/cssl-loading-alt.gif';
 
 interface LoadingIndicatorProps {
     message: string;
 }
 
-// TODO: Light mode
 const LoadingIndicator: React.FC<LoadingIndicatorProps> = ({ message }) => {
+  const [theme] = useTheme();
   return (
     <div className="flex flex-row gap-2 text-xs items-center">
-      <img src={csslLoading} className="w-6 h-6" alt="Loading spinner" />
+      <img src={theme === 'dark' ? csslLoading : csslLoadingAlt} className="w-6 h-6" alt="Loading spinner" />
       {message}
     </div>
   );
